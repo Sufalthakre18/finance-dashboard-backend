a17ac2dd3f7e6283194f337a307be58cf297e33d
sufalthakre <sufal@tellistechnologies.com>
2026-04-03 05:45:43 +0530

    add MongoDB connection configuration

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import "dotenv/config"
+
+import express from 'express'
+import cors from 'cors'
+import { connectDB } from "./config/db.js";
+
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Finance Backend API is running 🚀",
+        version: "1.0.0",
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`\n Server running on http://localhost:${PORT}`);
+});
