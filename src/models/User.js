f90dc4877d9887927231ddabb42d0c5b25d75ff8
sufalthakre <sufal@tellistechnologies.com>
2026-04-03 14:13:41 +0530

    create user model with roles and status, create transactional record model

---
+++ src/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+      select: false, // never return password in queries
+    },
+    role: {
+      type: String,
+      enum: ["viewer", "analyst", "admin"],
+      default: "viewer",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+ 
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+ 
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
